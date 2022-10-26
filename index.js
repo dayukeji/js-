41f2d0ec0b74568be76539a6d5d83ec5df49fa59
dayukeji <huliml@126.com>
2022-10-26 15:59:13 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// 定义全局变量foo
+var aa = 'foo1';
+
+fn = () => console.log('ssdsds')
+ctx = {
+  fn: (val) => console.log(val),
+  aa: '111'
+}
+
+const sand = (ctx, fnStr) => {
+  with(window){
+    eval(fnStr)
+  }
+}
+
+sand(ctx, 'fn(aa)')
